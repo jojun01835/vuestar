7c67425c535ecaedf1a585f8074b6cc127f4a5e1
jojun0 <jojun01835@gmail.com>
2023-09-12 18:16:15 +0900

    첫 커밋

---
+++ src/components/Container.vue
@@ -0,0 +1,19 @@
+<template lang="">
+  <div>
+    <Post :data="data" />
+  </div>
+</template>
+<script>
+import Post from "./Post.vue";
+export default {
+  name: "ContainerView",
+
+  props: {
+    data: Object,
+  },
+  components: {
+    Post,
+  },
+};
+</script>
+<style lang=""></style>
